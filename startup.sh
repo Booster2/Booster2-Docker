c3c236aed547d0d2d368c208fe5403b38fd63031
Seyyed Shah <seyyed.shah@cs.ox.ac.uk>
2017-03-07 10:33:24 +0000

    changes to make MYSQL run under root, db creation was failing (mysql user does not exist)

--- startup.sh
+++ startup.sh
@@ -18,7 +18,7 @@ echo "log_bin_trust_function_creators=1" >> /etc/mysql/my.cnf
 
 if [ ! -d "/run/mysqld" ]; then
 	mkdir -p /run/mysqld
-	chown -R mysql:mysql /run/mysqld
+	chown -R root:root /run/mysqld
 fi
 
 if [ -d /var/lib/mysql/mysql ]; then
@@ -26,9 +26,9 @@ if [ -d /var/lib/mysql/mysql ]; then
 else
 	echo "[i] MySQL data directory not found, creating initial DBs"
 
-	chown -R mysql:mysql /var/lib/mysql
+	chown -R root:root /var/lib/mysql
 
-	mysql_install_db --user=mysql > /dev/null
+	mysql_install_db --user=root > /dev/null
 
 	if [ "$MYSQL_ROOT_PASSWORD" = "" ]; then
 		MYSQL_ROOT_PASSWORD=""
@@ -63,7 +63,7 @@ EOF
 	    fi
 	fi
 
-	/usr/bin/mysqld --user=mysql --bootstrap --verbose=0 < $tfile
+	/usr/bin/mysqld --user=root --bootstrap --verbose=0 < $tfile
 	rm -f $tfile
 fi
 
@@ -77,7 +77,7 @@ do
 	fi
 done
 
-/usr/bin/mysqld --user=mysql --console --log-bin-trust-function-creators &
+/usr/bin/mysqld --user=root --console --log-bin-trust-function-creators &
 
 
 
