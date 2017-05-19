bfd0ed55e8e63b1b1601475765af524dd3622dc2
Seyyed Shah <seyyed.shah@cs.ox.ac.uk>
2017-05-19 17:06:29 +0100

    new run targets: 1) to not overwrite the database on startup 2) to update only methods and constraints without overwriting the database

--- startup.sh
+++ startup.sh
@@ -87,30 +87,61 @@ mkdir /Booster2/sql-gen
 
 cp /booster2/Booster2/sql-gen/standardStuff.sql /Booster2/sql-gen
 
+#if there are two arguments to the script, and the second arg is "--no-overwrite"
+#check the db has been created, then do nothing (everything should be in place for the GWI).
+if [ "$#" -eq 2 ] && [ "$2" == "--no-overwrite" ]; then
+if [ ! -f /files/`basename $1 boo2`generated.sql ]; then
 
-echo "Booster file: $1"
+echo "database has not been created for $1, try running without --no-overwrite"
+
+else
+
+echo "starting with existing database for the spec $1"
+
+fi
+fi
+
+#if there are two arguments to the script, and the second arg is "--methods-only"
+#check the db has been created, , do constraint generation only
+if [ "$#" -eq 2 ] && [ "$2" == "--methods-only" ]; then
+if [ ! -f /files/`basename $1 boo2`generated.sql ]; then
+
+echo "database has not been created for $1, try running without --methods-only"
+
+else
+
+if [ ! -f /files/$1 ]; then 
+echo "/files/$1 does not exist, create it and try again"
+
+else
+
+echo "regenerating methods and constraints on the existing database for the spec $1"
+
+java -jar /sunshine/sunshine.jar transform -n "Generate SQL methods" -p /files/ -l /booster2/Booster2/ -i $1
+#if we are only overwriting methods, the sql is different
+SQL_FILE_NAME=`basename $1 boo2`methods.generated.sql
+
+mysql -u root < /files/$SQL_FILE_NAME
+
+
+fi
+fi
+fi
+
+#if there is only one argument to the script, assume the database should be recreated, data import run and WARs deployed etc
+if [ "$#" -eq 1 ]; then
+if [ ! -e /files/$1 ]; then 
+
+echo "ERROR : /files/$1 does not exist, create it and try again"
+
+else
+
+echo "[i] using Booster file: $1"
 java -jar /sunshine/sunshine.jar transform -n "Generate SQL" -p /files/ -l /booster2/Booster2/ -i $1
 
 
 SQL_FILE_NAME=`basename $1 boo2`generated.sql
-
 mysql -u root < /files/$SQL_FILE_NAME
-
-DB_NAME=$(grep -i "^create database" /files/$SQL_FILE_NAME | grep -o "\`[^\`]*\`" | tr -d '`')
-
-mkdir /usr/local/tomcat/webapps/${DB_NAME} 
-
-unzip -d /usr/local/tomcat/webapps/${DB_NAME} /booster2/gwi.war
-
-sed -i "s-<dbname>IPG</dbname>-<dbname>${DB_NAME}</dbname>-g" /usr/local/tomcat/webapps/${DB_NAME}/WEB-INF/dbConfig.xml 
-
-sed -i "s-> James Welch <-> ${DB_NAME} User<-g" /usr/local/tomcat/webapps/${DB_NAME}/index.html
-
-sed -i "s-gwi-${DB_NAME}-g" /usr/local/tomcat/webapps/${DB_NAME}/js/script.js
-
-rm -rf /usr/local/tomcat/webapps/ROOT
-
-ls -s /usr/local/tomcat/webapps/ROOT /usr/local/tomcat/webapps/booster
 
 for f in /files/sql-import/*.sql
 do
@@ -118,35 +149,59 @@ do
   mysql -u root < $f
 done
 
-#use booster to generate a triple map
-echo "generating triple map: $1"
-java -jar /sunshine/sunshine.jar transform -n "Generate Triple Map" -p /files/ -l /booster2/Booster2/ -i $1
-
-#set up the dbname for the d2rq server
-sed -i "s-jdbc:mysql://localhost:3306/IPG-jdbc:mysql://localhost:3306/${DB_NAME}-g" /usr/local/tomcat/webapps/d2rq/WEB-INF/web.xml 
-
-#set the url/localport in the same file, as needed
-sed -i "s-http://localhost:8081/d2rq/-http://localhost:80/d2rq/-g" /usr/local/tomcat/webapps/d2rq/WEB-INF/web.xml 
-
-# copy the generated mapping file to D2RQ's web-inf dir
-cp /files/`basename $1 boo2`mapping.ttl /usr/local/tomcat/webapps/d2rq/WEB-INF/mapping.ttl
-
-#datadump for rdfunit 
-TTL_FILE_NAME=`basename $1 boo2`datadump.ttl
-cd /d2rq/d2rq/
-chmod a+x dump-rdf 
-#put a data dump in /files too, incase the users want it
-bash ./dump-rdf -j jdbc:mysql://localhost:3306/${DB_NAME} -u root -p "" -o /files/${TTL_FILE_NAME} -f TURTLE -b file:/${TTL_FILE_NAME}/ /usr/local/tomcat/webapps/d2rq/WEB-INF/mapping.ttl
-
-bash ./dump-rdf -j jdbc:mysql://localhost:3306/${DB_NAME} -u root -p "" -o /usr/local/tomcat/webapps/d2rq/data.n3 -b file:/data.n3/ /usr/local/tomcat/webapps/d2rq/WEB-INF/mapping.ttl
-
 cd /
 
+touch $1.success
+
+fi
+fi
+
+#the db will have been generated from
+SQL_FILE_NAME=`basename $1 boo2`generated.sql
+echo "SQL FILE NAME $SQL_FILE_NAME"
+
+DB_NAME=$(grep -i "^ create database" /files/$SQL_FILE_NAME | grep -o "\`[^\`]*\`" | tr -d '`')
+
+echo "DB_NAME $DB_NAME"
+
+mkdir /usr/local/tomcat/webapps/${DB_NAME} 
+
+unzip -d /usr/local/tomcat/webapps/${DB_NAME} /booster2/gwi.war
+
+sed -i "s-<dbname>IPG</dbname>-<dbname>${DB_NAME}</dbname>-g" /usr/local/tomcat/webapps/${DB_NAME}/WEB-INF/dbConfig.xml 
+sed -i "s-<dbname>IPG</dbname>-<dbname>${DB_NAME}</dbname>-g" /usr/local/tomcat/webapps/gwi/WEB-INF/dbConfig.xml 
+
+sed -i "s-> James Welch <-> ${DB_NAME} User<-g" /usr/local/tomcat/webapps/${DB_NAME}/index.html
+sed -i "s-> James Welch <-> ${DB_NAME} User<-g" /usr/local/tomcat/webapps/gwi/index.html
+
+sed -i "s-gwi-${DB_NAME}-g" /usr/local/tomcat/webapps/${DB_NAME}/js/script.js
+
+rm -rf /usr/local/tomcat/webapps/ROOT
+
+ln -s /usr/local/tomcat/webapps/ROOT /usr/local/tomcat/webapps/booster
+
+#use booster to generate a triple map
+#echo "generating triple map: $1"
+#java -jar /sunshine/sunshine.jar transform -n "Generate Triple Map" -p /files/ -l /booster2/Booster2/ -i $1
+
+#set up the dbname for the d2rq server
+#sed -i "s-jdbc:mysql://localhost:3306/IPG-jdbc:mysql://localhost:3306/${DB_NAME}-g" /usr/local/tomcat/webapps/d2rq/WEB-INF/web.xml 
+
+#set the url/localport in the same file, as needed
+#sed -i "s-http://localhost:8081/d2rq/-http://localhost:80/d2rq/-g" /usr/local/tomcat/webapps/d2rq/WEB-INF/web.xml 
+
+# copy the generated mapping file to D2RQ's web-inf dir
+#cp /files/`basename $1 boo2`mapping.ttl /usr/local/tomcat/webapps/d2rq/WEB-INF/mapping.ttl
+
+#datadump for rdfunit 
+#TTL_FILE_NAME=`basename $1 boo2`datadump.ttl
+#cd /d2rq/d2rq/
+#chmod a+x dump-rdf 
+#put a data dump in /files too, incase the users want it
+#bash ./dump-rdf -j jdbc:mysql://localhost:3306/${DB_NAME} -u root -p "" -o /files/${TTL_FILE_NAME} -f TURTLE -b file:/${TTL_FILE_NAME}/ /usr/local/tomcat/webapps/d2rq/WEB-INF/mapping.ttl
+
+#bash ./dump-rdf -j jdbc:mysql://localhost:3306/${DB_NAME} -u root -p "" -o /usr/local/tomcat/webapps/d2rq/data.n3 -b file:/data.n3/ /usr/local/tomcat/webapps/d2rq/WEB-INF/mapping.ttl
+
+
 echo Starting Tomcat service...
-bash /usr/local/tomcat/bin/catalina.sh run > /dev/null &
-echo Tomcat service started.
-
-sleep 10 && tail -f /usr/local/tomcat/logs/catalina.*.log
-
-
-
+bash /usr/local/tomcat/bin/catalina.sh run
