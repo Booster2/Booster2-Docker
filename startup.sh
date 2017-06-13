74c785d64e3c4e26ec51e8751985402ed7e55dd8
Seyyed Shah <seyyed.shah@cs.ox.ac.uk>
2017-06-13 18:16:36 +0100

    expose database, made permission more robust

--- startup.sh
+++ startup.sh
@@ -31,15 +31,6 @@ else
 
 	mysql_install_db --user=root > /dev/null
 
-	if [ "$MYSQL_ROOT_PASSWORD" = "" ]; then
-		MYSQL_ROOT_PASSWORD=""
-		echo "[i] MySQL root Password: $MYSQL_ROOT_PASSWORD"
-	fi
-
-	MYSQL_DATABASE=${MYSQL_DATABASE:-""}
-	MYSQL_USER=${MYSQL_USER:-""}
-	MYSQL_PASSWORD=${MYSQL_PASSWORD:-""}
-
 	tfile=`mktemp`
 	if [ ! -f "$tfile" ]; then
 	    return 1
@@ -48,26 +39,17 @@ else
 	cat << EOF > $tfile
 USE mysql;
 FLUSH PRIVILEGES;
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
-UPDATE user SET password=PASSWORD("") WHERE user='root' AND host='%';
+CREATE USER 'root'@'localhost' IDENTIFIED BY '';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
 UPDATE user SET password=PASSWORD("") WHERE user='root' AND host='localhost';
 CREATE USER 'root'@'%' IDENTIFIED BY '';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+UPDATE user SET password=PASSWORD("") WHERE user='root' AND host='%';
 EOF
 
-	if [ "$MYSQL_DATABASE" != "" ]; then
-	    echo "[i] Creating database: $MYSQL_DATABASE"
-	    echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
 
-	    if [ "$MYSQL_USER" != "" ]; then
-		echo "[i] Creating user: $MYSQL_USER with password $MYSQL_PASSWORD"
-		echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
-	    fi
-	fi
+	/usr/bin/mysqld --user=root --bootstrap  < $tfile
 
-	/usr/bin/mysqld --user=root --bootstrap --verbose=0 < $tfile
-	rm -f $tfile
 fi
 
 # execute any pre-exec scripts, useful for images
@@ -85,6 +67,10 @@ done
 /usr/bin/mysqld --user=root --console --log-bin-trust-function-creators &
 
 
+	if [ -f "$tfile" ]; then
+		sleep 2
+		/usr/bin/mysql --force --user=root < $tfile
+	fi
 
 
 mkdir /Booster2
